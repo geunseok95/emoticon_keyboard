1398d9117ede99ba2ed92923f0ae206c9cbd4c9a
GeunseokJang <geunseok95@gmail.com>
2020-12-03 21:22:07 +0900

    first

---
+++ app/src/main/java/com/professionalandroid/apps/miniproject_platfarm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.professionalandroid.apps.miniproject_platfarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
